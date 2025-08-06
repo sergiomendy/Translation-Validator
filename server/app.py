056d432fe6197dd396e66609c810a2fb9880aba5
sergiomendy <mendysergewilson@gmail.com>
2025-08-06 14:35:49 +0000

    Some changes

--- server/app.py
+++ server/app.py
@@ -17,8 +17,8 @@ MONGODB_URL = "mongodb+srv://admin:admin@cluster0.n7jczep.mongodb.net/?retryWrit
 DATABASE_NAME = "translations_db"
 
 # MongoDB client
-client = None
-database = None
+client = AsyncIOMotorClient(MONGODB_URL)
+database = client[DATABASE_NAME]
 
 # Initialize database
 async def init_db():
